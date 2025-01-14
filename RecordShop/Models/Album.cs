5abf5a1c75791675a425275c51474452cf36154b
Nick Midmore <nmidmore@gmail.com>
2025-01-14 09:42:21 +0000

    add album model and dbcontext

---
+++ RecordShop/Models/Album.cs
@@ -0,0 +1,11 @@
+﻿namespace RecordShop.Models;
+
+public class Album
+{
+    public int Id { get; set; }
+    public string Title { get; set; }
+    public string Artist { get; set; }
+    public int Year { get; set; }
+    public string Genre { get; set; }
+    public string Description { get; set; }
+}
---
+++ RecordShop/ShopContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RecordShop.Models;
+
+namespace RecordShop;
+
+public class ShopContext : DbContext
+{
+    public DbSet<Album> Albums { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        base.OnConfiguring(optionsBuilder);
+    }
+}
