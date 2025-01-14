ad2f4fbeeea0cf44cd25045ba1b040327ea740bd
Nick Midmore <nmidmore@gmail.com>
2025-01-14 10:26:45 +0000

    add AlbumController scaffold

---
+++ RecordShop/Controllers/AlbumController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.JsonPatch;
+using Microsoft.AspNetCore.Mvc;
+using RecordShop.Models;
+using System.Reflection.Metadata.Ecma335;
+
+namespace RecordShop.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class AlbumController(AlbumRepository repo) : ControllerBase
+{
+    private readonly AlbumRepository _repository = repo;
+
+    [HttpGet]
+    public IActionResult GetAlbums()
+        => Ok(_repository.Index());
+
+    [HttpGet("{id}")]
+    public IActionResult GetAlbumById(int id)
+        => Ok(_repository.IndexById(id));
+
+    [HttpPost]
+    public IActionResult AddAlbum(Album album)
+        => Ok(_repository.AddAlbum(album));
+
+    [HttpPatch("{id}")]
+    public IActionResult UpdateAlbum(int id, [FromBody] JsonPatchDocument patch)
+        => Ok(_repository.UpdateAlbum(id, patch));
+
+    [HttpDelete("{id}")]
+    public IActionResult DeleteAlbum(int id)
+        => Ok(_repository.DeleteAlbum(id));
+
+}
